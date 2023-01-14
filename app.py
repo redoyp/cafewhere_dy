10328cfc64fbddf3e97176dde77c3db68353410e
redoyp <86337937+redoyp@users.noreply.github.com>
2023-01-14 16:54:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+
+
 # 서버가 정상적으로 작동하는지 확인
 # gttps : //url.com
 @app.route("/")
 def hello():
   return "Hello, Flask!"
+
+
+
 
 # https://url.com/coffee
 @app.route('/coffe', methods=['POST'])
@@ -35,6 +41,42 @@ def coffe(): # 함수 선언
   
   # 답변
   return jsonify(res)
+
+
+
+@app.route('/answer1',methods=['POST'])
+def ans1():
+    req = request.get_json()
+    content = req['userRequest']
+    content = content['utterance']
+    param= req['action']['detailParams']
+    answer1 = param['answer1']['value'] 
+    answer2 = param['answer2']['value']
+    
+    user_id = req['userRequest']['user']['id']
+    answerlist[user_id] = []
+    
+    answerlist[user_id].append(answer1)
+    answerlist[user_id].append(answer2)
+    
+    response = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                { 
+                    "simpleText": {
+                        "text": '{}'.format(answerlist[user_id]) 
+                    }
+                }
+            ]
+        }
+    }
+
+    return response
+
+
+
+
 
 # 메인 함수
 if __name__ == '__main__':
