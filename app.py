193791f28096955607000d48ffd6ceea78d0746a
redoyp <86337937+redoyp@users.noreply.github.com>
2023-01-14 23:03:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,9 @@ def coffe(): # 함수 선언
 @app.route('/')
 def hello():
     return "안녕하세요! 카페웨어 챗봇입니다! 만나서 반가워요. 밑의 버튼을 눌러주시거나 원하시는 정보를 입력해주세요."
+  
+  
+  
 @app.route('/keyboard')
 def keyboard():
     keyboard =  {
