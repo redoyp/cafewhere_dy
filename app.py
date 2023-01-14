0c25587d4c8e6092a6e74196d029a510289d069f
redoyp <86337937+redoyp@users.noreply.github.com>
2023-01-14 22:42:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,11 +42,31 @@ def coffe(): # 함수 선언
   
   # 답변
   return jsonify(res)
+# 이 라인을 기준으로 위에는 제가 따로 연습할려고 사용한 부분입니다...
+
+
 
 
 
 @app.route('/answer1',methods=['POST'])
+def coffeeTI_message():
+    dataReceive = request.get_json()
+    content = dataReceive['content']
+    
+    dataSend = {
+      "message" : {
+        "text" : "당신의 커피 취향을 알려드리는 커피TI 입니다!! 다음의 질문에 따라 답변해주세요!!"
+        "text" : "Q1. 나에게 있어 커피 브레이크는?"
+        "text" : "(1) 친구들과 수다 떨며 스트레스 푸는 시간"
+        "text" : "(2) 조용히 혼자만의 시간을 가지며 생각을 정리하는 시간"
+      }
+    }
+    
+    return jsonify(datasend)
+
+
 def ans1():
+  
     req = request.get_json()
     content = req['userRequest']
     content = content['utterance']
