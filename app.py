7123b8d8151bd44fd696ac474de298ac72b86626
redoyp <86337937+redoyp@users.noreply.github.com>
2023-01-15 11:08:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 answer = 0
 
 
-@app.route('/answer1',methods=['POST'])
+@app.route('/ans1',methods=['POST'])
 def ans1():
   prob1_buttons : ['친구들과 수다 떨며 스트레스 푸는 시간', '조용히 혼자만의 시간을 가지며 생각을 정리하는 시간']
+    
+  dataReceive = request.get_json()
+  user_input = dataReceive['content']
+    
   response = {
     "message" : {
       "text" : '나에게 있어 커피 브레이크는?'
@@ -19,6 +23,11 @@ def ans1():
       "buttons" : prob1_buttons
     }
   }
+  
+  if user_input == prob1_buttons[0]:
+    answer += 1
+  elif user_input == prob1_buttons[1]: 
+    answer += 2
     
   return response
 
