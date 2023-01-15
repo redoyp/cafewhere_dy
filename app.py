b2dc94f9968721502dcc4c550b733b4eedbaef5c
redoyp <86337937+redoyp@users.noreply.github.com>
2023-01-15 11:28:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,24 @@ answer = 0
 
 @app.route('/answer1',methods=['POST'])
 def ans1():
-  prob1_buttons : ['친구들과 수다 떨며 스트레스 푸는 시간', '조용히 혼자만의 시간을 가지며 생각을 정리하는 시간']
-  response = {
-    "message" : {
-      "text" : '나에게 있어 커피 브레이크는?'
-    },
-    "keyboard" : {
-      "type" : "buttons",
-      "buttons" : prob1_buttons
-    }
-  }
-    
-  return response
+  body = request.get_json()
+    print(body)
+    print(body['userRequest']['utterance'])
 
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "당신의 커피 취향을 알려드리는 커피TI입니다."
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
 
 
 
