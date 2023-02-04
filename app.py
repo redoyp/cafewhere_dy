33737c67d0b0019e20a04d4db6b11121ed59f24d
redoyp <86337937+redoyp@users.noreply.github.com>
2023-02-05 05:24:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,23 @@ app = Flask(__name__)
 @app.route('/answer1',methods=['POST'])
 def answer1():
     
-    content = request.get_json()                       # 사용자가 입력한 정보를 json형태로 가져옴
-    content = content['userRequest']['utterance']      # 입력한 발화를 가져옴
+    content = request.get_json()
+    content = content['userRequest']['utterance']
     
     if content == u"친구들과 수다 떨며 스트레스 푸는 시간":
-        response = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {	
-                        "simpleText": {
-                            "text": "test"
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : "테스트입니다."
                         }
                     }
                 ]
             }
         }
-
-    return jsonify(response)
+    return jsonify(dataSend)
 
 
 
