f162c5e93b4fc779d02861079c96139315fa06f7
redoyp <86337937+redoyp@users.noreply.github.com>
2023-02-05 05:21:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,19 @@ def answer1():
     content = request.get_json()                       # 사용자가 입력한 정보를 json형태로 가져옴
     content = content['userRequest']['utterance']      # 입력한 발화를 가져옴
     
-    response = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {	
-                    "simpleText": {
-                        "text": "test"
+    if content == u"친구들과 수다 떨며 스트레스 푸는 시간"
+        response = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {	
+                        "simpleText": {
+                            "text": "test"
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
-    }
 
     return response
 
