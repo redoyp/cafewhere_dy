970d34ee8b71cb7e96c8ea255e1a15945b3f4a3f
redoyp <86337937+redoyp@users.noreply.github.com>
2023-02-05 05:21:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def answer1():
     content = request.get_json()                       # 사용자가 입력한 정보를 json형태로 가져옴
     content = content['userRequest']['utterance']      # 입력한 발화를 가져옴
     
-    if content == u"친구들과 수다 떨며 스트레스 푸는 시간"
+    if content == u"친구들과 수다 떨며 스트레스 푸는 시간":
         response = {
             "version": "2.0",
             "template": {
