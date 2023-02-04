3b9e24b15944dbbf63e176b4e2775bb71a5eb38b
redoyp <86337937+redoyp@users.noreply.github.com>
2023-02-04 16:06:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ app = Flask(__name__)
 answerlist = {}
 
 
-@app.route('/answer1',methods=['POST'])
+@app.route('/prob1',methods=['POST'])
 def ans1():
     req = request.get_json()
     content = req['userRequest']
     content = content['utterance']
     param= req['action']['detailParams']
-    answer1 = param['ans1-1']['value'] 
-    answer2 = param['ans1-2']['value']
+    answer1-1 = param['ans1-1']['value'] 
+    answer1-2 = param['ans1-2']['value']
     
     user_id = req['userRequest']['user']['id']
     answerlist[user_id] = []
     
-    answerlist[user_id].append(answer1)
-    answerlist[user_id].append(answer2)
+    answerlist[user_id].append(answer1-1)
+    answerlist[user_id].append(answer1-2)
     
     response = {
         "version": "2.0",
