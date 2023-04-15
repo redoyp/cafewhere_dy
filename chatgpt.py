e0acfb813027e4cf90e5f0ba8d4afde74661c927
redoyp <86337937+redoyp@users.noreply.github.com>
2023-04-15 21:39:47 +0900

    Create chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import openai
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+@app.route('/kakaotalk_chatbot_skill', methods=['POST'])
+def kakaotalk_chatbot_skill():
+    user_message = request.json['userRequest']['utterance']
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{user_message}\n",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+
+    reply_message = response.choices[0].text.strip()
+    
+    return jsonify({
+        'version': '2.0',
+        'template': {
+            'outputs': [
+                {
+                    'simpleText': {
+                        'text': reply_message
+                    }
+                }
+            ]
+        }
+    })
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
