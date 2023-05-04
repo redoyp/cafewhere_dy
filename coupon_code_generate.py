cdc6019494f08aba8006aee007ce909cdcf974e7
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-05 07:22:56 +0900

    Create coupon_code_generate.py

---
+++ coupon_code_generate.py
@@ -0,0 +1,21 @@
+import random
+import coupon
+def to_base62(n) :
+    codec = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    index = n % 62
+    code = codec[index]
+    return code
+
+def code_generate() :
+    num = random.randint(1, 62)
+    return num
+
+def coupon_code() :
+    string = []
+
+    for i in range(12) :
+        num = code_generate()
+        num = to_base62(num)
+        string.append(num)
+
+    return "".join(string)
