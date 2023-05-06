4e03bfc5450640fc37f66970cf9cc71247b9feaa
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-07 03:37:56 +0900

    Update coupon_count.py

--- coupon_count.py
+++ coupon_count.py
@@ -7,12 +7,15 @@ def count(cafe_name) :
         count[0] -= 1
         if count[0] == -1 :
             return -2
+        return count[0]
     elif cafe_name == '투썸플레이스' :
         count[1] -= 1
         if count[1] == -1 :
             return -2
+        return count[1]
     elif cafe_name == '이디야' :
         count[2] -= 1
         if count[2] == -1 :
             return -2
+        return count[2]
         
