c768dfbe23854046faec86e991e8b716a92c2962
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-07 00:59:31 +0900

    Update coupon_db.py

--- coupon_db.py
+++ coupon_db.py
@@ -116,6 +116,8 @@ def getCouponCode(user_id, cafe_name) : # 쿠폰 코드 출력 용 + 유저가 �
 
 if __name__ == '__main__' : ## coupon code generate test
     print(getCoupon())
+    insCoupon('1111', '2222', '3333')
+    print(getCoupon())
     delCoupon('1111', '2222', '3333')
     print(getCoupon())
     
