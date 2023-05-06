6121e86ae902edb342bc303061fc7ee5748bf4e4
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-07 02:32:23 +0900

    Update coupon_db.py

--- coupon_db.py
+++ coupon_db.py
@@ -150,11 +150,7 @@ def getCouponIDCafe(user_id, cafe_name) : # 유저가 쿠폰 뽑았는지 확인
 
     conn.commit()
     conn.close()
-    
-    if ret == None :
-        return 0
-    else :
-        return 1
+    return ret
 
 
 if __name__ == '__main__' : ## mysql test
