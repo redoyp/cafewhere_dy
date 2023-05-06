01a620cfc2a97fcd97263f2192f360700801a441
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-07 03:28:24 +0900

    Create coupon_count.py

---
+++ coupon_count.py
@@ -0,0 +1,19 @@
+def count(cafe_name) :
+    count = [100, 200, 300] # 0 : 스타벅스, 1 : 투썸플레이스, 2: 이디야
+                            # -> 각 카페의 쿠폰 개수 세기
+                            # 한정된 개수를 넘길시 쿠폰 소진 알리기 위함
+
+    if cafe_name == '스타벅스' :
+        count[0] -= 1
+        if count[0] == -1 :
+            return -2
+    elif cafe_name == '투썸플레이스' :
+        count[1] -= 1
+        if count[1] == -1 :
+            return -2
+    elif cafe_name == '이디야' :
+        count[2] -= 1
+        if count[2] == -1 :
+            return -2
+        
+    return count
