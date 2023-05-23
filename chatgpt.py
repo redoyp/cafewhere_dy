8e19f88894e72a5b7b9a1ae31b7abbcec8a99ab6
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-23 20:55:05 +0900

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,15 +1,43 @@
-import os
 import openai
+from config import *
+import time
+openai.api_key = OPENAI_API_KEY
 
-openai.api_key = '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'
+print("chatGPT를 종료하려면 'bye'를 입력하세요.\n")
+content = ''
 
-question = input("무엇을 물어볼까요?: ")
+while True:
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": question}
-  ]
-)
+    prompt = input("명령을 내려 주세요: ")
+    try:
+        messages = [
+                {'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': content},
+            ]
+        messages.append({'role': 'assistant', 'content': msg })
 
-print(completion.choices[0].message.content)
+        messages.append({'role': 'user', 'content': prompt })
+    except:
+        messages = [
+                {'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': prompt},
+            ]
+
+    if prompt == 'bye':
+        print("chatGPT를 종료합니다.")
+        break
+
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+
+    print(response)
+    print("--------------------------------")
+    print(str(response['choices'][0]['message']['content']).strip())
+    msg = str(response['choices'][0]['message']['content']).strip()
+    print("--------------------------------")
+
+    time.sleep(1)
+
+    content = content + msg
