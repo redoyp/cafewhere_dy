d97e78186dad14f57be556147945030eef49ea80
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-23 20:37:37 +0900

    is it run chatgpt?

--- chatgpt.py
+++ chatgpt.py
@@ -1,36 +1,17 @@
-from flask import Flask, request, jsonify
 import openai
 
-app = Flask(__name__)
-openai.api_key = ""
+openai.api_key = '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'
 
-@app.route('/kakaotalk_chatbot_skill', methods=['POST'])
-def kakaotalk_chatbot_skill():
-    user_message = request.json['userRequest']['utterance']
+messages = []
+while True:
+  content = input("User: ")
+  messages.append({"role":"user", "content":content})
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"{user_message}\n",
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.8,
-    )
+  completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+  )
 
-    reply_message = response.choices[0].text.strip()
-    
-    return jsonify({
-        'version': '2.0',
-        'template': {
-            'outputs': [
-                {
-                    'simpleText': {
-                        'text': reply_message
-                    }
-                }
-            ]
-        }
-    })
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+  chat_response = completion.choices[0].message.content
+  print(f'ChatGPT: {chat_response}')
+  messages.append({"role":"assistant", "content": chat_response})
