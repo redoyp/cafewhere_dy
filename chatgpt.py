42a7b23fc4a33c768f775a42a999f32766d1209e
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-23 22:28:10 +0900

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,17 +1,14 @@
+import os
 import openai
 
-openai.api_key  = ''
+openai.api_key =""
+question = input("무엇을 물어볼까요?: ")
 
-messages = []
-while True:
-  content = input("User: ")
-  messages.append({"role":"user", "content":content})
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": question}
+  ]
+)
 
-  completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=messages
-  )
-
-  chat_response = completion.choices[0].message.content
-  print(f'ChatGPT: {chat_response}')
-  messages.append({"role":"assistant", "content": chat_response})
+print(completion.choices[0].message.content)
