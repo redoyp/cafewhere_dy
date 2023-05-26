18860d4d3a7b5cdbb0e965cc3e7523254518bd0c
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-27 01:18:49 +0900

    Update chatgpt_response.py

--- chatgpt_response.py
+++ chatgpt_response.py
@@ -8,7 +8,7 @@ def chatgpt_response():
     
     req = request.get_json()
     content = req['userRequest']['utterance']
-    question = req['action']['params']['research']
+    question = req['action']['params']
     print(content, question)
     
     datasend = {
