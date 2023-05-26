c3ae0c73d9a8ed40cfb1ccc116077d9ee152a345
redoyp <86337937+redoyp@users.noreply.github.com>
2023-05-27 03:57:26 +0900

    Update gpt_model.py

--- gpt_model.py
+++ gpt_model.py
@@ -9,7 +9,7 @@ def gpt(question) :
     messages=[
       {"role": "user", "content": question}
     ],
-    max_tokens = 2048
+    max_tokens = 100
   )
 
   answer = completion.choices[0].message.content
